b26e5f8be2b064e520f35239833e6bde3a8d77ff
mpsloan <sloan.40@wright.edu>
2023-11-22 21:57:27 -0500

    iterator through csv and valid dict

--- main.py
+++ main.py
@@ -7,61 +7,65 @@ import pandas as pd
 
 def load_data():
     data_path = ""
-    csv_path = os.path.join(data_path, "nfl_data.csv")
+    csv_path = os.path.join(data_path, "nfl_data_edit.csv")
     return pd.read_csv(csv_path)
 
 
 nfl_data = load_data()
 
-teams = {"ARI": "Arizona Cardinals", "ATL": "Atlanta Falcons", "BAL": "Baltimore Ravens", "BUF": "Buffalo Bills",
-         "CAR": "Carolina Panthers", "CHI": "Chicago Bears", "CIN": "Cincinnati Bengals", "CLE": "Cleveland Browns",
-         "DAL": "Dallas Cowboys", "DEN": "Denver Broncos", "DET": "Detroit Lions", "GB": "Green Bay Packers",
-         "HOU": "Houston Texans", "IND": "Indianapolis Colts", "JAX": "Jacksonville Jaguars",
-         "KC": "Kansas City Chiefs", "LVR": "Las Vegas Raiders", "LAC": "Los Angeles Chargers",
-         "LAR": "Los Angeles Rams", "MIA": "Miami Dolphins", "MIN": "Minnesota Vikings", "NE": "New England Patriots",
-         "NO": "New Orleans Saints", "NYG": "New York Giants", "NYJ": "New York Jets", "PHI": "Philadelphia Eagles",
-         "PIT": "Pittsburgh Steelers", "SF": "San Francisco 49ers", "SEA": "Seattle Seahawks",
-         "TB": "Tampa Bay Buccaneers", "TEN": "Tennessee Titans", "WAS": "Washington Redskins",
-         }
+teams = {"Arizona Cardinals": "ARI", "Atlanta Falcons": "ATL", "Baltimore Colts": "IND", "Baltimore Ravens": "BAL",
+         "Boston Patriots": "NE", "Buffalo Bills": "BUF", "Carolina Panthers": "CAR", "Chicago Bears": "CHI",
+         "Cincinnati Bengals": "CIN", "Cleveland Browns": "CLE", "Dallas Cowboys": "DAL", "Denver Broncos": "DEN",
+         "Detroit Lions": "DET", "Green Bay Packers": "GB", "Houston Oilers": "TEN", "Houston Texans": "HOU",
+         "Indianapolis Colts": "IND", "Jacksonville Jaguars": "JAX", "Kansas City Chiefs": "KC",
+         "Las Vegas Raiders": "LVR", "Los Angeles Chargers": "LAC", "Los Angeles Raiders": "LVR",
+         "Los Angeles Rams": "LAR", "Miami Dolphins": "MIA", "Minnesota Vikings": "MIN", "New England Patriots": "NE",
+         "New Orleans Saints": "NO", "New York Giants": "NYG", "New York Jest": "NYJ", "Oakland Raiders": "LVR",
+         "Philadelphia Eagles": "PHI", "Phoenix Cardinals": "ARI", "Pittsburgh Steelers": "PIT",
+         "San Diego Chargers": "LAC", "San Francisco 49ers": "SF", "Seattle Seahawks": "SEA",
+         "St. Louis Cardinals": "ARI", "St. Louis Rams": "LAR", "Tampa Bay Buccaneers": "TB", "Tennessee Oilers": "TEN",
+         "Tennessee Titans": "TEN", "Washington Commanders": "WAS", "Washington Football Team": "WAS",
+         "Washington Redskins": "WAS"}
 
-reader = csv.reader(nfl_data, delimiter=',')
 i = 0
+with open("nfl_data_edit.csv", 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        for column in datareader:
+            team_home = column[4]
+            score_home = float(column[5])
+            score_away = float(column[6])
+            team_away = column[7]
+            team_fav_id = column[8]
+            spread_fav = float(column[9])
+            if team_fav_id == team_home:
+                home_fav = True
+            else:
+                away_fav = True
+                home_fav = False
 
-with open('nfl_data.csv', 'r') as inp, open('nfl_data_edit.csv', 'w') as out:
-    writer = csv.writer(out)
-    for row in csv.reader(inp):
-        if i == 0:
-            writer.writerow(row)
-        elif 2501 < i <= 13594:
-            writer.writerow(row)
-        i += 1
+            if home_fav:
+                difference = score_home - score_away - spread_fav
+            else:
+                difference = score_away - score_home - spread_fav
+
+            if difference > 0:
+                cover = 1
+            else:
+                cover = 0
+            #i += 1
+
+# with open('nfl_data.csv', 'r') as inp, open('nfl_data_edit.csv', 'w') as out:
+#     writer = csv.writer(out)
+#     for row in csv.reader(inp):
+#         if i == 0:
+#             writer.writerow(row)
+#         elif 2501 < i <= 13594:
+#             writer.writerow(row)
+#         i += 1
 
 
 
-# for column in reader:
-#     print(column, end=" ")
-#     print(i)
-#     i += 1
-
-
-# home_fav = false
-# away_fav = false
-# if team_fav_id = team_home
-    # home_fav = true
-# else
-    # away_fav = true
-
-# cover = 0
-# difference = 0
-# if home_fav
-    # difference = score_home - score_away - spread_favorite
-# else
-    # difference = score_away - score_home - spread_favorite
-
-# if difference > 0
-    # cover = 1
-# else
-    # cover = 0
 
 # if __name__ == '__main__':
 #     print("Main")
